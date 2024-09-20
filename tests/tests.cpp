049319ffd3e19618778287c980e41f9ccc728859
Software9 <dds2508@gmail.com>
2024-09-20 17:26:58 +0300

    Update tests.cpp

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -3,6 +3,6 @@
 #include<vector>
 
 TEST(ATEST, BTEST) {
-  vector<int> nums = { -1, 2, 1, -4 };
-    EXPECT_EQ(2, threeSumClosest({ -1, 2, 1, -4 }, 1));
+
+    EXPECT_EQ(2, threeSumClosest({-1, 2, 1, -4 }, 1));
 }
